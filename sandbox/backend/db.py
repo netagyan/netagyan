a1510430811b440c93efd49ea28a02555936f841
Kameshwar Nayak <kameshwar.snayak@gmail.com>
2013-06-22 21:06:56 +0530

    added select, insert and update functions

--- sandbox/backend/db.py
+++ sandbox/backend/db.py
@@ -12,11 +12,38 @@ class db:
     self.connection = MySQLdb.connect(DBHOST, DBUSER, DBPASS, DBNAME)
     self.cursor     = self.connection.cursor()
 
-  def runquery(self, query):
+  # insert rows into db
+  def insert(self, query):
     try:
       # Execute the SQL command
       self.cursor.execute(query)
-      db.commit()
+      self.connection.commit()
+      return true
     except:
       # Rollback in case there is any error
       db.rollback()
+      return false
+
+  # select from database
+  def select(self, query):
+    try:
+      # Execute the SQL command
+      self.cursor.execute(query)
+      # Fetch all the rows in a list of lists and return.
+      return self.cursor.fetchall()
+    except:
+      # Rollback in case there is any error
+      db.rollback()
+      return false
+
+  # update rows in db
+  def update(self, query):
+    try:
+      # Execute the SQL command
+      self.cursor.execute(query)
+      self.connection.commit()
+      return true
+    except:
+      # Rollback in case there is any error
+      db.rollback()
+      return false
