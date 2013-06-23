bd732b16754eafd545868a6fb61b99a839d8fca9
root <root@tila.(none)>
2013-06-23 08:18:57 +0530

    updated the script

--- sandbox/chromeextension/findthestring.js
+++ sandbox/chromeextension/findthestring.js
@@ -24,6 +24,15 @@ function ajaxrequest(requrl, reqdata, successcallback) {
 function displaydetails(data) {
   console.log("displaydetails");
   console.log(data);
+  var tooltip = $("<div id='tooltip'>").appendTo("body");
+  var table   = $("<table>").appendTo(tooltip).attr("style","width:400px;");
+  var trheader= $("<tr>");
+  var trdata  = $("<tr><br/>");
+  table.append(trheader).append(trdata);
+  $.each(data[0], function(index, value) {
+    trheader.append($("<td>").text(index));
+    trdata.append($("<td>").text(value));
+  });
 }
 
 function updatewords(data) {
@@ -55,7 +64,7 @@ function updatewords(data) {
 function spanaround(word) {
   $.each(elements, function(index, element) {;
     var stringsarray = $(element).html().toString();
-    var htmlstring = stringsarray.replace(new RegExp(word, "gi"), ("<span class='netagyan'>" + word + "</span>"));
+    var htmlstring = stringsarray.replace(new RegExp(word, "gi"), ("<span class='netagyan'>" + word + "</span>" + "<img class=\"explore\" src='http://www.usask.ca/fsd/images/icons/help.gif'>"));
     $(element).empty().html(htmlstring);
   })
   $.each($("span.netagyan"), function() {
